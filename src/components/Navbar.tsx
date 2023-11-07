6c5d7af49c82f04135d3d05fa1974419b1ae910a
RaiYashPramod <raiyashpramod16@gmail.com>
2023-11-07 11:59:01 +0530

    Navigation update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,40 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [toggle, toggleNav] = useState(false);
 
   return (
     <nav className="bg-gray-900 py-4 px-8 flex justify-between items-center">
-      <a href="/" className="text-[#e63946] font-epic text-3xl">
+      <Link to="/" className="text-[#e63946] font-epic text-3xl">
         M <br />M{" "}
-      </a>
+      </Link>
       <ul className={`lg:flex space-x-4 hidden lg:items-center`}>
         <li className="flex flex-row space-x-12">
-          <a
-            href="/battle-ground"
+          <Link
+            to="/battle-ground"
             className="block text-[#e63946] text-xl font-mono hover:border-b-2  hover:border-[#e63946]"
           >
             Battle Ground
-          </a>
-          <a
-            href="/preferred-movies"
+          </Link>
+          <Link
+            to="/preferred-movies"
             className="block text-[#e63946] text-xl font-mono hover:border-b-2  hover:border-[#e63946]"
           >
             What to watch
-          </a>
-          <a
-            href="/create-battle"
+          </Link>
+          <Link
+            to="/create-battle"
             className="block text-[#e63946] text-xl font-mono hover:border-b-2  hover:border-[#e63946]"
           >
             Create Battle's
-          </a>
-          <a
-            href="/arena"
+          </Link>
+          <Link
+            to="/arena"
             className="block text-[#e63946] text-xl font-mono hover:border-b-2  hover:border-[#e63946]"
           >
             Arena
-          </a>
+          </Link>
         </li>
       </ul>
       <button
@@ -60,30 +61,30 @@ const Navbar = () => {
         <div className="fixed inset-0 bg-gray-900 bg-opacity-75 h-screen w-screen flex justify-center items-center z-50 transition-opacity duration-300" onClick={() => toggleNav(!toggle)}>
           <ul className="text-white text-xl text-center">
             <li className="flex flex-col space-y-10">
-              <a
-                href="/battle-ground"
+              <Link
+                to="/battle-ground"
                 className="text-[#e63946] text-3xl font-semibold font-mono hover:border-b-2  hover:border-[#e63946]"
               >
                 Battle Ground
-              </a>
-              <a
-                href="/preferred-movies"
+              </Link>
+              <Link
+                to="/preferred-movies"
                 className="block text-[#e63946] text-3xl font-semibold font-mono hover:border-b-2  hover:border-[#e63946]"
               >
                 What to watch
-              </a>
-              <a
-                href="/create-battle"
+              </Link>
+              <Link
+                to="/create-battle"
                 className="block text-[#e63946] text-3xl font-semibold font-mono hover:border-b-2  hover:border-[#e63946]"
               >
                 Create Battle's
-              </a>
-              <a
-                href="/arena"
+              </Link>
+              <Link
+                to="/arena"
                 className="block text-[#e63946] text-3xl font-semibold font-mono hover:border-b-2  hover:border-[#e63946]"
               >
                 Arena
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
